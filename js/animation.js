ed5d11ae1ab26d262e396002e685d291ef6b2d57
Amir <stha.amir57@gmail.com>
2021-05-26 21:20:37 +0545

    responsive fix

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,10 @@
-// const today = new Date();
 const gdm = document.querySelector(".good-morning");
 const ahile = today.getHours();
 const tl = gsap.timeline();
 
-if (ahile <= 6 && ahile >= 3) {
+// ahile <= 6 && ahile >= 3
+
+if (1) {
   gdm.style.visibility = "visible";
 
   tl.from(".good-morning svg", 1, {
@@ -15,24 +16,29 @@ if (ahile <= 6 && ahile >= 3) {
       amount: 0.4,
     },
   });
+
+  tl.to(
+    ".good-morning svg",
+    1,
+    {
+      x: 200,
+      opacity: 0,
+      ease: "SlowMo.ease.config(0.7, 0.7, false)",
+    },
+    "+=1.5"
+  );
 }
 
-tl.from(".day", 1, {
-  opacity: 0,
-  ease: "SlowMo.ease.config(0.7, 0.7, false)",
-  delay: 1,
-  scaleX: 0,
-});
-
-tl.to(
-  ".good-morning svg",
+tl.from(
+  ".day",
   1,
   {
-    x: 200,
     opacity: 0,
     ease: "SlowMo.ease.config(0.7, 0.7, false)",
+    delay: 1,
+    scaleX: 0,
   },
-  "-=1"
+  "-=2"
 );
 
 tl.from(
@@ -83,18 +89,20 @@ tl.from(
   1.8,
   {
     x: 2000,
+    opacity: 0,
     ease: "power4.out",
     delay: 0,
     // scale: 45,
     // skewY: 15,
   },
-  "-=1.9"
+  "-=1.8"
 );
 tl.from(
   ".inner-bar-1",
   1.8,
   {
     x: 2000,
+    opacity: 0,
     ease: "power4.out",
     delay: 0,
     // scale: 45,
