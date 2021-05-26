e76e1d7cdd2e78372e03736e38c4f951f72b7b5f
Amir <stha.amir57@gmail.com>
2021-05-26 18:42:20 +0545

    first commit

---
+++ js/animation.js
@@ -0,0 +1,130 @@
+// const today = new Date();
+const gdm = document.querySelector(".good-morning");
+const ahile = today.getHours();
+const tl = gsap.timeline();
+
+if (ahile <= 6 && ahile >= 3) {
+  gdm.style.visibility = "visible";
+
+  tl.from(".good-morning svg", 1, {
+    y: 200,
+    ease: "power4.out",
+    delay: 1,
+    skewY: 15,
+    stagger: {
+      amount: 0.4,
+    },
+  });
+}
+
+tl.from(".day", 1, {
+  opacity: 0,
+  ease: "SlowMo.ease.config(0.7, 0.7, false)",
+  delay: 1,
+  scaleX: 0,
+});
+
+tl.to(
+  ".good-morning svg",
+  1,
+  {
+    x: 200,
+    opacity: 0,
+    ease: "SlowMo.ease.config(0.7, 0.7, false)",
+  },
+  "-=1"
+);
+
+tl.from(
+  ".fly",
+  3.2,
+  {
+    x: -2200,
+    opacity: 0,
+    ease: "Expo.easeInOut",
+    delay: 0,
+    scaleX: 1.7,
+  },
+  "-=1.2"
+);
+
+tl.from(
+  ".day .today-is span",
+  1.8,
+  {
+    y: 200,
+    ease: "power4.out",
+    delay: 0,
+    skewY: 15,
+    stagger: {
+      amount: 0.4,
+    },
+  },
+  "-=2.6"
+);
+
+tl.from(
+  "li a",
+  1.8,
+  {
+    y: 400,
+    ease: "power4.out",
+    delay: 0,
+    skewY: 15,
+    stagger: {
+      amount: 0.2,
+    },
+  },
+  "-=1.8"
+);
+
+tl.from(
+  ".inner-bar-2",
+  1.8,
+  {
+    x: 2000,
+    ease: "power4.out",
+    delay: 0,
+    // scale: 45,
+    // skewY: 15,
+  },
+  "-=1.9"
+);
+tl.from(
+  ".inner-bar-1",
+  1.8,
+  {
+    x: 2000,
+    ease: "power4.out",
+    delay: 0,
+    // scale: 45,
+    // skewY: 15,
+  },
+  "-=0.2"
+);
+tl.from(
+  ".asmt",
+  1.8,
+  {
+    // x: 500,
+    opacity: 0,
+    ease: "power4.out",
+    delay: 0,
+    // scale: 45,
+    // skewY: 15,
+  },
+  "-=0.8"
+);
+tl.from(
+  ".sem",
+  1.8,
+  {
+    // x: 500,
+    opacity: 0,
+    ease: "power4.out",
+    delay: 0,
+    // scale: 45,
+    // skewY: 15,
+  },
+  "-=3.8"
+);
---
+++ js/app.js
@@ -0,0 +1,68 @@
+const today = new Date();
+const day = today.getDay();
+const todayIs = document.querySelector(".today-is");
+const UL = document.querySelector("ul");
+const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const subject = {
+  CG: {
+    title: "Computer Graphics",
+    link: "https://meet.google.com/zxw-vpbw-eas",
+  },
+  CA: {
+    title: "Computer Architecture",
+    link: "https://meet.google.com/fqy-wcve-qfk",
+  },
+  NM: {
+    title: "Numerical Method",
+    link: "https://meet.google.com/bdu-fgwz-rpu",
+  },
+  STAT: {
+    title: "Statistics II",
+    link: "https://meet.google.com/pht-ttpv-hyb",
+  },
+  DSA: {
+    title: "DSA",
+    // link: "https://meet.google.com/yak-ddjh-kcs",
+    // link: "https://meet.google.com/kmm-jckn-aah",
+    link: "https://meet.google.com/ssw-hkhf-oyk",
+  },
+};
+
+loadAllFunctions();
+
+function loadAllFunctions() {
+  display();
+}
+
+function display() {
+  if (day === 6) {
+    noClass(day);
+  } else if (day === 0 || day === 1 || day === 2) {
+    subFirst(day);
+  } else {
+    subLast(day);
+  }
+}
+
+function noClass(day) {
+  console.log("Today is HOliday");
+  alert("NO classes today");
+  let element = `<li><a href="#">Null</a></li>`;
+  UL.innerHTML = `${element}`;
+  todayIs.children[0].innerHTML = days[day];
+}
+
+function subFirst(day) {
+  let element = `<li><a href="${subject.CG.link}">${subject.CG.title}</a></li>
+        <li><a href="${subject.CA.link}">${subject.CA.title}</a></li>`;
+  UL.innerHTML = `${element}`;
+  todayIs.children[0].innerHTML = days[day];
+}
+
+function subLast(day) {
+  let element = `<li><a href="${subject.NM.link}">${subject.NM.title}</a></li>
+        <li><a href="${subject.STAT.link}">${subject.STAT.title}</a></li>
+        <li><a href="${subject.DSA.link}">${subject.DSA.title}</a></li>`;
+  UL.innerHTML = `${element}`;
+  todayIs.children[0].innerHTML = days[day];
+}
